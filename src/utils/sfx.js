285bcfd604a04af8e5969c435efe3150bc0cf4ac
its_swapnadip108 <Aethron-fr@users.noreply.github.com>
2026-06-17 19:35:10 +0530

    fix: mute annoying hover pop sound

--- src/utils/sfx.js
+++ src/utils/sfx.js
@@ -19,25 +19,8 @@ class SFXEngine {
   }
 
   playHoverTick() {
-    if (!this.initialized) this.init();
-    if (!this.ctx) return;
-
-    const osc = this.ctx.createOscillator();
-    const gain = this.ctx.createGain();
-
-    osc.type = 'sine';
-    osc.frequency.setValueAtTime(800, this.ctx.currentTime);
-    osc.frequency.exponentialRampToValueAtTime(1200, this.ctx.currentTime + 0.05);
-
-    gain.gain.setValueAtTime(0, this.ctx.currentTime);
-    gain.gain.linearRampToValueAtTime(0.2, this.ctx.currentTime + 0.01);
-    gain.gain.exponentialRampToValueAtTime(0.01, this.ctx.currentTime + 0.05);
-
-    osc.connect(gain);
-    gain.connect(this.masterGain);
-
-    osc.start(this.ctx.currentTime);
-    osc.stop(this.ctx.currentTime + 0.06);
+    // Silenced due to user feedback (pop sound was annoying)
+    return;
   }
 
   playTerminalKeystroke() {
