b586bd3790975b828fdc261d8b364cdd5479479d
its_swapnadip108 <Aethron-fr@users.noreply.github.com>
2026-06-25 12:36:39 +0530

    fix: remove velocity scroll tilt effect

--- src/components/ScrollSkewWrapper.jsx
+++ src/components/ScrollSkewWrapper.jsx
@@ -1,27 +1,4 @@
-import { useRef } from 'react';
-import { motion, useScroll, useVelocity, useTransform, useSpring } from 'framer-motion';
-
+// Tilt effect removed as per user request
 export default function ScrollSkewWrapper({ children }) {
-  const { scrollY } = useScroll();
-  const scrollVelocity = useVelocity(scrollY);
-  
-  // Smooth the velocity to prevent erratic jumping
-  const smoothVelocity = useSpring(scrollVelocity, {
-    damping: 50,
-    stiffness: 400
-  });
-
-  // Map velocity range to a subtle skew degree (limit to max +/- 3 degrees for elegance)
-  const skew = useTransform(smoothVelocity, [-1500, 1500], [2, -2]);
-
-  return (
-    <motion.div 
-      style={{ 
-        skewY: skew,
-        transformOrigin: 'center center'
-      }}
-    >
-      {children}
-    </motion.div>
-  );
+  return <>{children}</>;
 }
